d967f021e493d0a282ff6ecf2b02e257f9e44cc3
Basilio <basilioherimiharisoa@gmail.com>
2025-08-07 16:33:58 +0300

    Interacting With the Map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,20 +1,34 @@
 import styles from "./Map.module.css";
-// import {  useSearchParams } from "react-router-dom";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import { useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
+import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesProvider.jsx";
+import PropTypes from "prop-types";
 function Map() {
-  const [mapPosition] = useState([40.46635901755316, -3.7133789062500004]);
+  const [mapPosition, setMapPosition] = useState([
+    40.46635901755316, -3.7133789062500004,
+  ]);
   const { cities } = useCities();
-  // const [searchParams] = useSearchParams();
-  // const lat = searchParams.get("lat");
-  // const lng = searchParams.get("lng");
+  const [searchParams] = useSearchParams();
+  const mapLat = searchParams.get("lat");
+  const mapLng = searchParams.get("lng");
+
+  useEffect(() => {
+    if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+  }, [mapLng, mapLat]);
 
   return (
     <div className={styles.mapContainer}>
       <MapContainer
         center={mapPosition}
-        zoom={13}
+        zoom={6}
         scrollWheelZoom={true}
         className={styles.map}
       >
@@ -33,9 +47,31 @@ function Map() {
             </Popup>
           </Marker>
         ))}
+        <SetMapView position={mapPosition} />
+        <ClickMap />
       </MapContainer>
     </div>
   );
+}
+function SetMapView({ position }) {
+  const map = useMap();
+  useEffect(() => {
+    map.setView(position);
+  }, [position, map]);
+  return null;
+}
+SetMapView.propTypes = {
+  position: PropTypes.array,
+};
+
+function ClickMap() {
+  const navigate = useNavigate();
+  useMapEvents({
+    click: (e) => {
+      navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+    },
+  });
+  return null;
 }
 
 export default Map;
