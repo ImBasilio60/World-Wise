26dd82f6a83b0a59684d634c743c9e5123410ebc
Basilio <basilioherimiharisoa@gmail.com>
2025-08-07 05:43:59 +0300

    Ajout d'un carte geographique

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,23 +1,31 @@
 import styles from "./Map.module.css";
-import { useNavigate, useSearchParams } from "react-router-dom";
+// import {  useSearchParams } from "react-router-dom";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useState } from "react";
 function Map() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const [mapPosition] = useState([-18.766947, 46.869107]);
+  // const [searchParams] = useSearchParams();
+  // const lat = searchParams.get("lat");
+  // const lng = searchParams.get("lng");
 
-  const navigate = useNavigate();
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <h1>
-        Map {lat} : {lng}
-      </h1>
-      <button
-        onClick={() => {
-          setSearchParams({ lat: "25", lng: "26" });
-        }}
+    <div className={styles.mapContainer}>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
       >
-        Set
-      </button>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
