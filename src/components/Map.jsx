df3be74e97a877f346a299cedde30eebe03fb75e
Basilio <basilioherimiharisoa@gmail.com>
2025-08-07 16:57:59 +0300

    Setting Map Position With Geolocation

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -11,12 +11,20 @@ import {
 import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesProvider.jsx";
 import PropTypes from "prop-types";
+import { useGeolocation } from "../hooks/useGeolocation.js";
+import Button from "./Button.jsx";
 function Map() {
   const [mapPosition, setMapPosition] = useState([
     40.46635901755316, -3.7133789062500004,
   ]);
   const { cities } = useCities();
   const [searchParams] = useSearchParams();
+  const {
+    isLoading: isLoadingMap,
+    position: geolocationPosition,
+    getPosition,
+  } = useGeolocation();
+
   const mapLat = searchParams.get("lat");
   const mapLng = searchParams.get("lng");
 
@@ -24,6 +32,13 @@ function Map() {
     if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
   }, [mapLng, mapLat]);
 
+  useEffect(() => {
+    if (geolocationPosition) {
+      setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+    }
+  }, [geolocationPosition]);
+
+  console.log(mapPosition);
   return (
     <div className={styles.mapContainer}>
       <MapContainer
@@ -36,6 +51,9 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
+        <Button type={"position"} onClick={getPosition}>
+          {isLoadingMap ? "Loading..." : "USE YOUR POSITION"}
+        </Button>
         {cities.map((city) => (
           <Marker
             position={[city.position.lat, city.position.lng]}
